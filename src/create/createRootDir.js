d33f7b433a552a110c6df6a8ca862df8d93666d4
Paul Heintzelman <pheintzelman@gmail.com>
2021-06-07 08:22:37 -0700

    Added tests

--- src/create/createRootDir.js
+++ src/create/createRootDir.js
@@ -1,25 +1,28 @@
 import fsModule from 'fs';
 const fs = fsModule.promises;
+import path from 'path';
 import { DefaultAppName } from '../constants.js';
 import { logger } from '../logger.js';
 import { dirExists } from '../lib/file.js';
 
 // If the dir already exists append the next number
-async function getDirName(name, count = 0) {
+async function getRootDirName(baseDir, name, count = 0) {
   const dir = count === 0 ? name : `${name}${count}`;
+  const rootDir = path.join(baseDir, dir);
 
-  if ((await dirExists(dir)) && count <= 100) {
-    return getDirName(name, count + 1);
+  if ((await dirExists(rootDir)) && count <= 100) {
+    return getRootDirName(baseDir, name, count + 1);
   }
 
-  return dir;
+  return rootDir;
 }
 
 export async function createRootDir(config) {
   const name = config.name ?? DefaultAppName;
-  const dir = await getDirName(name);
+  const baseDir = config.dir ?? '';
+  const rootDir = await getRootDirName(baseDir, name);
 
-  await fs.mkdir(dir);
-  logger.debug(`Dir created: ${dir}`);
-  return dir;
+  await fs.mkdir(rootDir);
+  logger.debug(`Dir created: ${rootDir}`);
+  return rootDir;
 }
